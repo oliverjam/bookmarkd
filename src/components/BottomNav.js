c17962226f38e765967306fffdefe7f80cacaffa
Akin909 <akin.sowemimo@gmail.com>
2017-05-04 17:46:06 +0100

    Add Links to bottom nav

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import { Nav } from './utilities/Nav';
 
 const BottomNavigation = styled(Nav)`
   color: green;
+  justify-content: space-between;
 `;
 
-const BottomNav = () => <BottomNavigation>Footer</BottomNavigation>;
+const BottomNav = () => (
+  <BottomNavigation>
+    <Link to="/">Link</Link>
+    <Link to="/">Link</Link>
+    <Link to="/">Link</Link>
+  </BottomNavigation>
+);
 
 export default BottomNav;
--- src/components/utilities/Nav.js
+++ src/components/utilities/Nav.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
 export const Nav = styled.header`
+  display: flex;
   background-color: skyblue;
   width: 100%;
+  padding: 0.3rem 0;
 `;
