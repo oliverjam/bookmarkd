ba9be7eee61df9c7880d7bac22c02ece83b7c49f
Akin909 <akin.sowemimo@gmail.com>
2017-05-05 16:11:02 +0100

    Removed small section of readme unnecessarily
    Attempt to get user page and re-render the viewer

--- src/components/Reader.js
+++ src/components/Reader.js
@@ -12,45 +12,48 @@ const ReaderContainer = styled.div`
   border: 1px solid black;
 `;
 
+// location={'epubcfi(/6/2[cover]!/6)'} // TODO figure out serving first page
 class Reader extends Component {
-  state = {
-    location: '',
-  };
+  // state = {
+  //   location: '',
+  // };
 
   componentDidMount() {
     this.props.addBook({
+      location: 'epubcfi(/6/10[chapter_002]!/4/2/16/1:120)',
       slug: this.props.slug,
     });
   }
 
-  componentWillReceiveProps() {
-    const currentLocation = (this.props.user.books.find(
-      book => book.slug === this.props.slug
-    ) || {}).location;
+  // componentWillReceiveProps() {
+  //   const currentLocation = (this.props.user.books.find(
+  //     book => book.slug === this.props.slug
+  //   ) || {}).location;
 
-    this.setState({
-      // location: currentLocation,
-      location: 'epubcfi(/6/10[chapter_002]!/4/2/16/1:120)',
-    });
-    // console.log(currentLocation);
-  }
+  // this.setState({
+  //   // location: currentLocation,
+  //   location: 'epubcfi(/6/10[chapter_002]!/4/2/16/1:120)',
+  // });
+  // }
 
   onLocationChange = location => {
-    // console.log(location);
-    this.props.updateCurrentLocation({ slug: this.props.slug, location });
+    console.log('location change', location);
+    this.forceUpdate();
+    // this.props.updateCurrentLocation({ slug: this.props.slug, location });
   };
 
   render() {
-    const loc = 'epubcfi(/6/10[chapter_002]!/4/2/16/1:120)';
-    console.log(this.state.location === loc);
+    const { books } = this.props.user;
+    const home = 'epubcfi(/6/2[cover]!/6)';
+    const location = books[0] && books[0].location ? books[0].location : home;
+    console.log('location', location);
     return (
       <ReaderContainer>
         <ReactReader
           url={`https://s3-eu-west-1.amazonaws.com/react-reader/${this.props.slug}.epub`}
           title={'Alice in wonderland'}
-          // location={'epubcfi(/6/2[cover]!/6)'} // TODO figure out serving first page
-          location={this.state.location} // TODO figure out serving first page
-          // location={loc} // TODO figure out serving first page
+          // location={'epubcfi(/6/10[chapter_002]!/4/2/16/1:120)'} // TODO figure out serving first page
+          location={location}
           locationChanged={this.onLocationChange}
         />
       </ReaderContainer>
