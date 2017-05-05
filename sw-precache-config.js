d43bac52213b2eae42e210ae0cb6c4b64f4ec54c
oliverjam <oliverjamesphillips@gmail.com>
2017-05-05 16:20:48 +0100

    Precache all assets in public, rather than specifying paths

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,10 +1,5 @@
 module.exports = {
   stripPrefix: 'build/',
-  staticFileGlobs: [
-    'build/*.html',
-    'build/manifest.json',
-    'build/static/**/!(*map*)'
-  ],
   dontCacheBustUrlsMatching: /\.\w{8}\./,
-  swFilePath: 'build/service-worker.js'
+  swFilePath: 'build/service-worker.js',
 };
