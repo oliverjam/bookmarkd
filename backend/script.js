f4c57bf7f3b4d73bb374c4198304a5633b258174
Zooey <zooey@lbk.org.uk>
2017-05-08 14:50:19 +0100

    add prompt for directory path I've changed the node script slightly so now it asks the user for a path to a directory full of epubs and we use that directory for making the objects. relates to #38

--- backend/script.js
+++ backend/script.js
@@ -1,35 +1,46 @@
 const fs = require('fs');
 const unzipper = require('unzipper');
 const xmlParser = require('xml2json');
+const uuid = require('uuid/v4');
+const stdio = require('stdio');
 
-const bookArr = fs.readdirSync(process.argv[2]);
-
-let count = 0;
-
-const bookInfo = [];
-
-bookArr.forEach(book => {
-  const stream = fs.createReadStream(`./epubs/${book}`);
-  stream.pipe(unzipper.Parse()).on('entry', function(entry) {
-    var fileName = entry.path;
-    if (fileName === 'OEBPS/content.opf') {
-      let data = '';
-      entry.on('data', chunk => (data += chunk)).on('end', () => {
-        const bookJson = JSON.parse(xmlParser.toJson(data));
-        const metadata = bookJson.package.metadata;
-        const returnObj = {
-          title: metadata['dc:title'],
-          author: metadata['dc:creator']['$t'],
-          slug: book.slice(0, book.length - 5),
-        };
-        count++;
-        bookInfo.push(returnObj);
-        if (count === bookArr.length) {
-          console.log(bookInfo);
-        }
-      });
-    } else {
-      entry.autodrain();
-    }
-  });
+let dir = '.';
+stdio.question('Enter path of directory you want to read', (err, res) => {
+  if (err) throw err;
+  makeBookArr(res);
 });
+
+function makeBookArr(dir) {
+  const bookArr = fs.readdirSync(dir);
+
+  let count = 0;
+
+  const bookInfo = [];
+
+  bookArr.forEach(book => {
+    const stream = fs.createReadStream(`${dir}/${book}`);
+    stream.pipe(unzipper.Parse()).on('entry', function(entry) {
+      var fileName = entry.path;
+      if (fileName === 'OEBPS/content.opf') {
+        let data = '';
+        entry.on('data', chunk => (data += chunk)).on('end', () => {
+          const bookJson = JSON.parse(xmlParser.toJson(data));
+          const metadata = bookJson.package.metadata;
+          const returnObj = {
+            title: metadata['dc:title'],
+            author: metadata['dc:creator']['$t'],
+            slug: book.slice(0, book.length - 5),
+            id: uuid(),
+          };
+          count++;
+          bookInfo.push(returnObj);
+          if (count === bookArr.length) {
+            console.log(bookInfo);
+          }
+        });
+      } else {
+        entry.autodrain();
+      }
+    });
+  });
+}
