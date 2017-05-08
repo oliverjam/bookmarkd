0cde9f23df13850d60b1db50eb0d8d77f8687766
Oliver <oliverjamesphillips@gmail.com>
2017-05-08 16:45:57 +0100

    Remove typo

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -14,7 +14,7 @@ module.exports = {
   staticFileGlobs: [
     'build/*.html',
     'build/manifest.json',
-    'build/static/**/!(*map*)',
+    'build/static/**/!(*map)',
   ],
   dontCacheBustUrlsMatching: /\.\w{8}\./,
   swFilePath: 'build/service-worker.js',
