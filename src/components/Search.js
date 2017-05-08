c93564fd614c5eb33f71aba8b56af06bafd60b38
Akin909 <akin.sowemimo@gmail.com>
2017-05-08 16:23:04 +0100

    Add initial fuzzy searching

---
+++ src/components/Search.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: '',
+    };
+  }
+  handleChange = event => {
+    this.setState({
+      value: event.target.value,
+    });
+    const searchResults = this.props.books.filter(book => {
+      const searchTerm = this.state.value.toLowerCase();
+      return (
+        book.author.toLowerCase().includes(searchTerm) ||
+        book.title.toLowerCase().includes(searchTerm)
+      );
+    });
+  };
+
+  render() {
+    return (
+      <input
+        value={this.state.value}
+        onChange={this.handleChange}
+        placeholder="search here"
+      />
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    books: state.books,
+  };
+}
+
+export default connect(mapStateToProps)(Search);
--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import Search from './Search';
 
 import Nav from './utilities/Nav';
 
@@ -9,7 +10,12 @@ const TopNavigation = styled(Nav)`
 `;
 
 function TopNav() {
-  return <TopNavigation><Link to="/">Bookmarkd</Link></TopNavigation>;
+  return (
+    <TopNavigation>
+      <Link to="/">Bookmarkd</Link>
+      <Search />
+    </TopNavigation>
+  );
 }
 
 export default TopNav;
