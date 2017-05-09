9e0f18b87b1e98a13c75fb0f90ff133d935dd306
Akin909 <akin.sowemimo@gmail.com>
2017-05-09 12:26:42 +0100

    Add redux offline configuration to index.js

--- src/index.js
+++ src/index.js
@@ -3,23 +3,26 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import promiseMiddleware from 'redux-promise';
+import { offline } from 'redux-offline';
+import offlineConfig from 'redux-offline/lib/defaults';
 
 import rootReducer from './reducers';
 import App from './components/App';
 import './index.css';
 import defaultState from './defaultState.js';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(
-  rootReducer,
-  defaultState,
-  composeEnhancers(applyMiddleware(promiseMiddleware)),
+const enhancer = compose(
+  applyMiddleware(promiseMiddleware),
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__(),
+  offline(offlineConfig)
 );
+
+const store = createStore(rootReducer, defaultState, enhancer);
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
