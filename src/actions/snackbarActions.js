97975a76c0263f7cb1bdff8726f0d4c98f3a610a
Zooey <zooey@lbk.org.uk>
2017-05-09 14:42:00 +0100

    I don't think i changed anything?

--- src/actions/snackbarActions.js
+++ src/actions/snackbarActions.js
@@ -1,6 +1,8 @@
 import { SNACKBAR_MESSAGE } from '../constants';
 
-export const snackbarMessage = ({ message }) => ({
-  type: SNACKBAR_MESSAGE,
-  message,
-});
+export const snackbarMessage = ({ message }) => {
+  return {
+    type: SNACKBAR_MESSAGE,
+    message,
+  };
+};
