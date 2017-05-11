b44bea21638a2ea09d0abcf971eff5e0afc36a0e
Akin909 <akin.sowemimo@gmail.com>
2017-05-11 14:19:49 +0100

    Add initial app intro copy

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,11 @@ function Home() {
   return (
     <PageLayout>
       <h1>Books</h1>
+      <p>
+        <section>Welcome to Bookmarkd 🤓!</section>
+        We aim to provide a free, open source platform to read, and eventually upload books.
+        You can browse our library of available books, and download these books onto your local machine to peruse later.
+      </p>
       <BookRows />
     </PageLayout>
   );
