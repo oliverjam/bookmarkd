b66eecc96fcdc0e296e75f77db7b51767958498b
oliverjam <oliverjamesphillips@gmail.com>
2017-05-12 10:34:04 +0100

    Use grid rather than row for library layout
    Relates #105

--- src/components/base/Grid.js
+++ src/components/base/Grid.js
@@ -1,12 +1,33 @@
 import styled from 'styled-components';
 
-const Grid = styled.ul`
-  max-width: 72em;
-  margin: 0 auto;
-  display: grid;
-  grid-template-columns: repeat(auto-fit, 12em);
-  grid-gap: ${props => props.theme.spaceM};
-  justify-content: center;
+export const Grid = styled.ul`
+  margin-top: -${props => props.theme.spaceS};
+  margin-left: -${props => props.theme.spaceS};
+  display: flex;
+  flex-wrap: wrap;
+  @media (min-width: 40em) {
+    margin-top: -${props => props.theme.spaceM};
+    margin-left: -${props => props.theme.spaceM};
+  }
 `;
 
-export default Grid;
+export const GridItem = styled.li`
+  padding-top: ${props => props.theme.spaceS};
+  padding-left: ${props => props.theme.spaceS};
+  flex: 0 0 50%
+  height: 14em;
+  @media (min-width: 40em) {
+    padding-top: ${props => props.theme.spaceM};
+    padding-left: ${props => props.theme.spaceM};
+    flex-basis: 33%;
+  }
+  @media (min-width: 55em) {
+    flex-basis: 25%;
+  }
+  @media (min-width: 70em) {
+    flex-basis: 20%;
+  }
+  @media (min-width: 85em) {
+    flex-basis: 16.6666%;
+  }
+`;
